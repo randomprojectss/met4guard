eba109c2b6c817b609619d40a26a088084114afb
TheGreatSageThatEqualToHeaven <phillippkociolek@gmail.com>
2024-09-29 16:22:04 +0200

    Update scripttt.lua

--- scripttt.lua
+++ scripttt.lua
@@ -1,8 +1,3 @@
--- Lade die mainFunction von der URL
-local url = 'https://met4guards.netlify.app/script.lua'  -- Die URL des Skripts
-
--- Lade die Funktion
-local mainFunction = loadstring(game:HttpGet(url))()  -- Die Hauptfunktion wird hier geladen
-
--- Übergib den scriptKey an die Hauptfunktion
-mainFunction(scriptKey)  -- Hier wird der Schlüssel direkt übergeben
+return function(scriptKey) 
+local githubFunction = loadstring(game:HttpGet('https://met4guards.netlify.app/script.lua'))()
+githubFunction(scriptKey)
